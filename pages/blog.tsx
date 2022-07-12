9362bda293c5b734ef337c45f21a0415184163c0
Abheek Dhawan <abheekd@protonmail.com>
2022-07-12 11:57:59 -0500

    fix: attempt to set correct paths and imports for MDX files

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,25 +1,39 @@
-import type { NextPage } from "next";
-import Box from "../components/box";
-import { Metadata } from "../components/articleLayout";
-import { promises as fs } from "node:fs";
-import path from "path";
-import Link from "next/link";
+import type { NextPage } from 'next';
+import Box from '../components/box';
+import { Metadata } from '../components/articleLayout';
+import { promises as fs } from 'node:fs';
+import path from 'path';
+import Link from 'next/link';
 
 export const getStaticProps = async () => {
-  const articleFiles = await fs.readdir(process.cwd() + "\\pages\\blog");
+  const articleFiles = await fs.readdir(
+    path.join(process.cwd(), 'pages', 'blog')
+  );
+  const postModules = await Promise.all(
+    articleFiles.map(async (p) => import(`./blog/${p}`))
+  );
+  const postMetadata = postModules.map((m) => (m.meta ? m.meta : null));
+  return {
+    props: {
+      articles: postMetadata,
+    },
+  };
+  /*
+  console.log(fs.readFileSync('terminal-customization.mdx', 'utf-8'));
+  console.log(import(process.cwd() + '\\terminal-customization.mdx'));
   const data = new Array<Metadata>();
   const tags = new Set<string>();
 
   for (const file of articleFiles) {
-    await import(`${__dirname}/blog/${file}`).then(
-      ({ meta }: { meta: Metadata }) => {
-        meta.slug = path.parse(file).name;
-        data.push(meta);
-        meta.tags.forEach((tag) => {
-          tags.add(tag);
-        });
-      }
-    );
+    if (!path.parse(file).ext.endsWith('mdx')) continue;
+    import(file).then(({ meta }: { meta: Metadata }) => {
+      console.log(meta);
+      meta.slug = path.parse(file).name;
+      data.push(meta);
+      meta.tags.forEach((tag) => {
+        tags.add(tag);
+      });
+    });
   }
 
   return {
@@ -27,7 +41,7 @@ export const getStaticProps = async () => {
       articles: data,
       tags: Array.from(tags),
     },
-  };
+  };*/
 };
 
 const Blog = ({
@@ -44,11 +58,11 @@ const Blog = ({
         {articles.map((article, idx) => {
           return (
             <div key={idx}>
-              <Link href={article.slug || "#"}>
-                <a>{article.title}</a>
-              </Link>{" "}
-              <span className={"text-gray-400 text-xs"}>
-                {new Date(article.timestamp).toLocaleString().split(",")[0]}
+              <Link href={article.slug || '#'}>
+                <a>{article.description}</a>
+              </Link>{' '}
+              <span className={'text-gray-400 text-xs'}>
+                {new Date(article.timestamp).toLocaleString().split(',')[0]}
               </span>
             </div>
           );
