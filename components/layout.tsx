3f362acc99226c24a3ccf4aabd2defaacb59f59e
Abheek Dhawan <abheekd@protonmail.com>
2022-07-13 15:13:17 -0500

    feat: add logos and metadata

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,6 +13,23 @@ export default function Layout({
     <>
       <Head>
         <title>{`${title} | ADAT`}</title>
+
+        {/* metadata */}
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta property='og:title' content={`${title} | ADAT`} />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://adawesome.tech' />
+        <meta
+          property='og:image'
+          content='https://adawesome.tech/img/logo.png'
+        />
+        <meta
+          property='og:description'
+          content={
+            "I'm Abheek, a passionate developer intent on learning everything there is to learn about many interesting technology- related topics. These include, but are not limited to: programming languages such as Go, C++, and TypeScript; the workings of OSes on a software level as well as the hardware counterpart; and finally the inner workings of different software, games, and other media and reverse engineering them. In addition, I run a YouTube channel where I post tech videos. I hope this website will serve as a hub for my many different endeavors and will allow for others to follow along or learn something."
+          }
+        />
+        <meta name='theme-color' content='#607dfa' />
       </Head>
       <Box>{children}</Box>
       {/*todo: add footer*/}
