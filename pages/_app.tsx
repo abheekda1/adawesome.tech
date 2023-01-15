46a7add60e3485e7039c3ecacf802d4e772e67dd
Abheek Dhawan <abheekd@protonmail.com>
2023-01-15 14:17:08 -0600

    refactor: rename MyApp to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/globals.css';
 import 'prism-themes/themes/prism-atom-dark.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default App;
